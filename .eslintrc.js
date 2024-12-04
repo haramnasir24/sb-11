87d232b357fbae91260ebc908116e49ceb140f01
Muhammad Wasif Shakeel <mshakeel.bsds23seecs@seecs.edu.pk>
2024-12-04 12:25:20 +0500

    feat(google sheets api + nodemailer): added mailing func + fetch/write db func

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "react/no-unescaped-entities": "off",
     "react-hooks/exhaustive-deps": "off",
-
+    "@typescript-eslint/no-explicit-any": "off",
     "react/display-name": "off",
     "react/jsx-curly-brace-presence": [
       "warn",
---
+++ src/constant/form-schemas/schema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+const Schema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.string().email("Invalid email format"),
+  phone: z.string().min(10, "Phone number must be at least 10 digits"),
+});
+
+export default Schema;
