b1cfe924838bd615c85296b30e6764e7e033812b
LemurSage <zainjee37405@gmail.com>
2024-12-22 18:31:42 +0500

    refactor(form): removed all abominations of code

--- src/app/register/layout.tsx
+++ src/app/register/layout.tsx
@@ -1,5 +1,12 @@
-import FormFooter from "@/components/form/footer";
-import FormNavbar from "@/components/form/navbar";
+import { Metadata } from "next";
+
+import FormFooter from "@/components/registration-form/common/footer";
+import FormNavbar from "@/components/registration-form/common/navbar";
+
+export const metadata: Metadata = {
+  title: "Science Bee | Registration",
+  description: "Register for Science Bee Gyaara",
+};
 
 export default function FormLayout({
   children,
