f2e22fc59b7f5959a0d305cd873c9874b9a090d8
LemurSage <zainjee37405@gmail.com>
2024-12-25 01:16:31 +0500

    fix(registration-form-header): responsiveness across all screen sizes

--- src/components/registration-form/common/registration-form-header.tsx
+++ src/components/registration-form/common/registration-form-header.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   FaClipboardList,
   FaMoneyCheckAlt,
@@ -20,15 +21,15 @@ const RegistrationHeader: React.FC<RegistrationHeaderProps> = ({
   ];
 
   return (
-    <div className="mb-12 bg-white p-6">
-      <h2 className="mb-6 text-center text-xl font-extrabold text-purple-800 md:text-3xl">
+    <div className="mb-4 bg-white p-3 sm:mb-8 sm:p-4 md:mb-12 md:p-6">
+      <h2 className="mb-4 text-center text-lg font-extrabold text-purple-800 sm:mb-6 sm:text-xl md:text-2xl lg:text-3xl">
         Science Bee Registration Form
       </h2>
       <div className="flex items-center justify-between">
         {steps.map((step, index) => (
           <div key={index} className="flex flex-1 flex-col items-center">
             <div
-              className={`flex h-9 w-9 items-center justify-center rounded-full border-4 md:h-12 md:w-12 ${
+              className={`sm:border-3 flex h-6 w-6 items-center justify-center rounded-full border-2 sm:h-8 sm:w-8 md:h-10 md:w-10 md:border-4 lg:h-12 lg:w-12 ${
                 index < currStep
                   ? "border-purple-800 bg-purple-800 text-white"
                   : index === currStep
@@ -36,9 +37,9 @@ const RegistrationHeader: React.FC<RegistrationHeaderProps> = ({
                     : "border-gray-300 bg-white text-gray-300"
               }`}
             >
-              <step.icon className="h-4 w-4 md:h-6 md:w-6" />
+              <step.icon className="h-3 w-3 sm:h-4 sm:w-4 md:h-5 md:w-5 lg:h-6 lg:w-6" />
             </div>
-            <div className="mt-2 h-1 w-full bg-gray-200">
+            <div className="mt-1 h-0.5 w-full bg-gray-200 sm:mt-2 sm:h-1">
               <div
                 className={`h-full ${
                   index < currStep ? "bg-purple-800" : "bg-gray-200"
@@ -54,9 +55,9 @@ const RegistrationHeader: React.FC<RegistrationHeaderProps> = ({
               />
             </div>
             <span
-              className={`mt-2 text-xs font-medium ${
+              className={`mt-1 text-[0.6rem] font-medium sm:mt-2 sm:text-xs md:text-sm ${
                 index <= currStep ? "text-purple-800" : "text-gray-500"
-              }`}
+              } hidden sm:inline-block`}
             >
               {step.label}
             </span>
